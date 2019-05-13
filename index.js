8ed77d5f0f59d1f39bed32bd72fdec8099800648
dwolfgram <daniel.j.wolfgram@gmail.com>
2019-05-12 23:57:55 -0400

    first commit

--- index.js
+++ index.js
@@ -3,17 +3,15 @@ const request = require('request')
 const networks = require('./networks')
 const MIN_RELAY_FEE = 1000
 const DEFAULT_SAT_PER_BYTE = 10
-const DERIVE_PATH = "m/49'/2'/0'"
 
-function SegwitDepositUtils (options) {
-  if (!(this instanceof SegwitDepositUtils)) return new SegwitDepositUtils(options)
+function LitecoinSegwitPayments (options) {
+  if (!(this instanceof LitecoinSegwitPayments)) return new LitecoinSegwitPayments(options)
   let self = this
   self.options = Object.assign({}, options || {})
   if (!self.options.insightUrl) {
     self.options.insightUrl = 'https://insight.litecore.io/api/'
     console.log('WARN: Using default bitcoin block explorer. It is highly suggested you set one yourself!', self.options.insightUrl)
   }
-
   if (!self.options.feePerKb) {
     self.options.feePerByte = DEFAULT_SAT_PER_BYTE
   }
@@ -30,85 +28,52 @@ function SegwitDepositUtils (options) {
   } else {
     throw new Error('Invalid network provided ' + self.options.network)
   }
-  // if (!self.options.password) throw new Error('SegwitDepositUtils: password required')
+  // if (!self.options.password) throw new Error('LitecoinSegwitPayments: password required')
   return self
 }
 
-SegwitDepositUtils.prototype.bip44 = function (xpub, path) {
-  let self = this
-  let node = bitcoin.HDNode.fromBase58(xpub, self.options.network)
-  let nodeDerivation = node.derive(0).derive(path)
-  let redeemScript = bitcoin.script.witnessPubKeyHash.output.encode(bitcoin.crypto.hash160(nodeDerivation.getPublicKeyBuffer()))
-  let scriptPubKey = bitcoin.script.scriptHash.output.encode(bitcoin.crypto.hash160(redeemScript))
-  return bitcoin.address.fromOutputScript(scriptPubKey, self.options.network)
+LitecoinSegwitPayments.prototype.getAddress = function(node, network) {
+  const wif = node.toWIF()
+  const keyPair = bitcoin.ECPair.fromWIF(wif, network)
+  let { address } = bitcoin.payments.p2sh({
+    redeem: bitcoin.payments.p2pkh({ pubkey: keyPair.publicKey })
+  })
+  const decoded = bitcoin.address.fromBase58Check(address)
+  address = bitcoin.address.toBase58Check(decoded['hash'], 50)
+  return address
 }
 
-SegwitDepositUtils.prototype.getPrivateKey = function (xprv, path) {
-  let self = this
-  if (!xprv) throw new Error('Xprv is null. Bad things will happen to you.')
-  // create the hd wallet
-  const node = bitcoin.HDNode.fromBase58(xprv, self.options.network)
-  let child = node.derivePath(DERIVE_PATH)
-  let nodeDerivation = child.derive(0).derive(path)
-  return nodeDerivation.keyPair.toWIF()
-}
-
-SegwitDepositUtils.prototype.privateToPublic = function (privateKey) {
-  let self = this
-  var keyPair = bitcoin.ECPair.fromWIF(privateKey, self.options.network)
-  let redeemScript = bitcoin.script.witnessPubKeyHash.output.encode(bitcoin.crypto.hash160(keyPair.getPublicKeyBuffer()))
-  let scriptPubKey = bitcoin.script.scriptHash.output.encode(bitcoin.crypto.hash160(redeemScript))
-  return bitcoin.address.fromOutputScript(scriptPubKey, self.options.network)
-}
-
-SegwitDepositUtils.prototype.generateNewKeys = function (entropy) {
-  let self = this
-  var root = bitcoin.HDNode.fromSeedHex(entropy, self.options.network)
-  return {
-    xprv: root.toBase58(),
-    xpub: self.getXpubFromXprv(root.toBase58())
-  }
-}
-
-SegwitDepositUtils.prototype.getXpubFromXprv = function (xprv) {
-  let self = this
-  let node = bitcoin.HDNode.fromBase58(xprv, self.options.network)
-  let child = node.derivePath(DERIVE_PATH)
-  // let derivedPubKey = key.derive("m/44'/60'/0'/0").hdPublicKey
-  return child.neutered().toBase58()
-}
-
-SegwitDepositUtils.prototype.getBalance = function (address, done) {
+LitecoinSegwitPayments.prototype.getBalance = function(address, options = {}, done) {
   let self = this
   let url = self.options.insightUrl + 'addr/' + address
-  request.get({json: true, url: url}, function (err, response, body) {
+  request.get({ json: true, url: url }, (err, response, body) => {
     if (!err && response.statusCode !== 200) {
-      return done(new Error('Unable to get balance from ' + url))
+      return done('Unable to get balance from ' + url)
     } else {
-      done(null, {balance: body.balance, unconfirmedBalance: body.unconfirmedBalance})
+      done(null, { balance: body.balance, unconfirmedBalance: body.unconfirmedBalance })
     }
   })
 }
 
-SegwitDepositUtils.prototype.getUTXOs = function (xpub, path, done) {
+LitecoinSegwitPayments.prototype.getUTXOs = function(node, network, done) {
   let self = this
-  let address = self.bip44(xpub, path)
-  // console.log('sweeping ', address)
+  let address = self.getAddress(node, network)
+  //console.log('getting utxos:', address)
   let url = self.options.insightUrl + 'addr/' + address + '/utxo'
-  request.get({json: true, url: url}, function (err, response, body) {
+  request.get({ json: true, url: url }, function(err, response, body) {
     if (!err && response.statusCode !== 200) {
-      return done(new Error('Unable to get UTXOs from ' + url + ' - status ' + response.statusCode))
+      return done('Unable to get UTXOs from ' + url)
     } else if (body.length === 0) {
-      return done(new Error('Unable to get UTXOs from ' + url + ' - empty response'))
+      return done('This address has no unspent outputs ' + url)
     } else {
       let cleanUTXOs = []
-      body.forEach(function (utxo) {
+      body.forEach(function(utxo) {
         delete utxo['confirmations']
         delete utxo['height']
         delete utxo['ts']
         cleanUTXOs.push(utxo)
       })
-      if (self.options.network === networks.testnet) {
+      if (self.options.network === bitcoin.networks.testnet) {
         console.log('TESTNET ENABLED: Clipping UTXO length to 2 for test purposes')
         cleanUTXOs = cleanUTXOs.slice(0, 2)
       }
@@ -116,24 +81,67 @@ SegwitDepositUtils.prototype.getUTXOs = function (xpub, path, done) {
     }
   })
 }
-SegwitDepositUtils.prototype.getSweepTransaction = function (xprv, path, to, utxo, feePerByte) {
+
+LitecoinSegwitPayments.prototype.broadcastTransaction = function(txObject, done, retryUrl, originalResponse) {
   let self = this
-  const txb = new bitcoin.TransactionBuilder(self.options.network)
+  let textBody = '{"rawtx":"' + txObject.signedTx + '"}'
+  const broadcastHeaders = {
+    'pragma': 'no-cache',
+    'cookie': '__cfduid=d365c2b104e8c0e947ad9991de7515e131528318303',
+    'origin': 'https://blockexplorer.com',
+    'accept-encoding': 'gzip, deflate, br',
+    'accept-language': 'en-US,en;q=0.9,fr;q=0.8,es;q=0.7',
+    'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36',
+    'content-type': 'application/json;charset=UTF-8',
+    'accept': 'application/json, text/plain, */*',
+    'cache-control': 'no-cache',
+    'authority': 'blockexplorer.com',
+    'referer': 'https://blockexplorer.com/tx/send'
+  }
+  let url
+  if (retryUrl) url = retryUrl
+  else url = self.options.insightUrl
+  var options = {
+    url: url + 'tx/send',
+    method: 'POST',
+    headers: broadcastHeaders,
+    body: textBody
+  }
+  request(options, error, response, body => {
+    if (!error && response.statusCode === 200) {
+      txObject.broadcasted = true
+      done(null, txObject)
+    } else {
+      if (url !== retryUrl) { // First broadcast attempt. Lets try again.
+        self.broadcastTransaction(txObject, done, self.options.backupBroadcastUrl, body)
+      } else {
+      // Second attempt failed
+        done('unable to broadcast. Some debug info: ' + body.toString() + ' ---- ' + originalResponse.toString())
+      }
+    }
+  })
+}
+
+LitecoinSegwitPayments.prototype.getTransaction = function(node, network, to, amount, utxo, feePerByte) {
+  let self = this
+  const txb = new bitcoin.TransactionBuilder(network)
   let totalBalance = 0
   if (utxo.length === 0) {
     return new Error('no UTXOs')
   }
-  utxo.forEach(function (spendable) {
+  utxo.forEach(function(spendable) {
     totalBalance += spendable.satoshis
     txb.addInput(spendable.txid, spendable.vout) // alice1 unspent
   })
   if (!feePerByte) feePerByte = self.options.feePerByte
   let txfee = estimateTxFee(feePerByte, utxo.length, 1, true)
   if (txfee < MIN_RELAY_FEE) txfee = MIN_RELAY_FEE
-  if ((totalBalance - txfee) < txfee) return new Error('Balance too small to sweep!' + totalBalance + ' ' + txfee)
-  txb.addOutput(to, totalBalance - txfee)
-  let keyPair = bitcoin.HDNode.fromBase58(xprv, self.options.network).derivePath(DERIVE_PATH).derive(0).derive(path).keyPair
-  let redeemScript = bitcoin.script.witnessPubKeyHash.output.encode(bitcoin.crypto.hash160(keyPair.getPublicKeyBuffer()))
+  if ((amount + txfee) > totalBalance) return new Error('Balance too small!' + totalBalance + ' ' + txfee)
+  txb.addOutput(to, amount - txfee)
+  const wif = node.toWIF()
+  const keyPair = bitcoin.ECPair.fromWIF(wif, network)
+  const pubKeyHash = bitcoin.crypto.ripemd160(bitcoin.crypto.sha256(keyPair.publicKey)).toString('hex')
+  const redeemScript = bitcoin.script.fromASM(`OP_DUP OP_HASH160 ${pubKeyHash} OP_EQUALVERIFY OP_CHECKSIG`)
   for (let i = 0; i < utxo.length; i++) {
     txb.sign(i,
       keyPair,
@@ -145,46 +153,20 @@ SegwitDepositUtils.prototype.getSweepTransaction = function (xprv, path, to, utx
   return { signedTx: txb.build().toHex(), txid: txb.build().getId() }
 }
 
-SegwitDepositUtils.prototype.broadcastTransaction = function (txObject, done, retryUrl, originalResponse) {
+LitecoinSegwitPayments.prototype.transaction = function(node, coin, to, amount, options = {}, done) {
   let self = this
-  let textBody = txObject.signedTx
-  let url
-  if (retryUrl) url = retryUrl
-  else url = 'https://ltc1.trezor.io/api/sendtx/'
-  var options = {
-    url: url,
-    method: 'POST',
-    body: textBody
-  }
-  request(options, function (error, response, body) {
-    if (!error && response.statusCode === 200) {
-      txObject.broadcasted = true
-      done(null, txObject)
-    } else {
-      if (url !== retryUrl) { // First broadcast attempt. Lets try again.
-        self.broadcastTransaction(txObject, done, self.options.backupBroadcastUrl, body)
-      } else {
-        // Second attempt failed
-        done(new Error('unable to broadcast. Some debug info: ' + body.toString() + ' ---- ' + originalResponse.toString()))
-      }
-    }
-  })
-}
-
-SegwitDepositUtils.prototype.sweepTransaction = function (xpub, xprv, path, to, feePerByte, done) {
-  let self = this
-  self.getUTXOs(xpub, path, function (err, utxo) {
+  self.getUTXOs(node, coin.network, (err, utxo) => {
     if (err) return done(err)
-    let signedTx = self.getSweepTransaction(xprv, path, to, utxo, feePerByte)
+    let signedTx = self.getTransaction(node, coin.network, to, amount, utxo, options.feePerByte)
     self.broadcastTransaction(signedTx, done)
   })
 }
 
 /**
- * Estimate size of transaction a certain number of inputs and outputs.
- * This function is based off of ledger-wallet-webtool/src/TransactionUtils.js#estimateTransactionSize
- */
-function estimateTxSize (inputsCount, outputsCount, handleSegwit) {
+* Estimate size of transaction a certain number of inputs and outputs.
+* This function is based off of ledger-wallet-webtool/src/TransactionUtils.js#estimateTransactionSize
+*/
+const estimateTxSize = function(inputsCount, outputsCount, handleSegwit) {
   var maxNoWitness,
     maxSize,
     maxWitness,
@@ -201,27 +183,27 @@ function estimateTxSize (inputsCount, outputsCount, handleSegwit) {
   }
   if (handleSegwit) {
     minNoWitness =
-      varintLength + 4 + 2 + 59 * inputsCount + 1 + 31 * outputsCount + 4
+    varintLength + 4 + 2 + 59 * inputsCount + 1 + 31 * outputsCount + 4
     maxNoWitness =
-      varintLength + 4 + 2 + 59 * inputsCount + 1 + 33 * outputsCount + 4
+    varintLength + 4 + 2 + 59 * inputsCount + 1 + 33 * outputsCount + 4
     minWitness =
-      varintLength +
-      4 +
-      2 +
-      59 * inputsCount +
-      1 +
-      31 * outputsCount +
-      4 +
-      106 * inputsCount
+    varintLength +
+    4 +
+    2 +
+    59 * inputsCount +
+    1 +
+    31 * outputsCount +
+    4 +
+    106 * inputsCount
     maxWitness =
-      varintLength +
-      4 +
-      2 +
-      59 * inputsCount +
-      1 +
-      33 * outputsCount +
-      4 +
-      108 * inputsCount
+    varintLength +
+    4 +
+    2 +
+    59 * inputsCount +
+    1 +
+    33 * outputsCount +
+    4 +
+    108 * inputsCount
     minSize = (minNoWitness * 3 + minWitness) / 4
     maxSize = (maxNoWitness * 3 + maxWitness) / 4
   } else {
@@ -240,4 +222,15 @@ function estimateTxFee (satPerByte, inputsCount, outputsCount, handleSegwit) {
   return mean * satPerByte
 }
 
-module.exports = SegwitDepositUtils
+LitecoinSegwitPayments.prototype.getFee = function(node, network, options = {}, done) {
+  let self = this
+  const feePerByte = options.feePerByte || self.options.feePerByte
+  self.getUTXOs(node, network, (err, utxo) => {
+    if (!err) {
+      return done(null, estimateTxFee(feePerByte, utxo.length, 1, true))
+    }
+    return done(err)
+  })
+}
+
+module.exports = LitecoinSegwitPayments
